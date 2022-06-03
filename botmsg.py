676afc0c038339ddc14a8187ae05ac3672dca067
brunovns <brunovinicius1196@gmail.com>
2022-06-03 04:39:36 -0300

    primeiro commit

---
+++ botmsg.py
@@ -0,0 +1,33 @@
+from asyncio.trsock import TransportSocket
+from re import T
+from tkinter import ttk
+from pip import main
+import pyautogui
+import time
+from threading import Thread
+from pynput import keyboard
+
+def main():
+    main.status = 'run'
+
+    #f = open("text", "r")
+    while True:
+
+        #for word in f:
+        while main.status == 'pause':
+            exit()
+
+        time.sleep(1.55)
+        pyautogui.typewrite('t')
+        pyautogui.press("enter")
+
+def exit_program():
+    def on_press(key):
+        if str(key) == 'Key.esc':
+            main.status = 'pause'
+            exit()
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+Thread(target=main).start()
+Thread(target=exit_program).start()
